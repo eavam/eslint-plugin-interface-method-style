24941ee8bbd75231426a5ad5b244167062b93936
Egor Avakumov <16797465+eavam@users.noreply.github.com>
2024-11-30 20:51:12 +0300

    refactor: simplify plugin configuration and remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,52 @@
 import interfaceMethodStyle from "./interface-method-style.js";
-import { name, version } from "../package.json";
-import * as typescriptParser from "@typescript-eslint/parser";
+// import { name, version } from "../package.json";
+// import * as typescriptParser from "@typescript-eslint/parser";
+import { type ESLint, type Linter } from "eslint";
+
+const namePlugin = "interface-method-style";
 
 const rules = {
   "interface-method-style": interfaceMethodStyle,
 };
 
-const flatConfig = {
+// const flatConfig = {
+//   plugins: {
+//     "interface-method-style": {
+//       meta: { name, version },
+//       rules,
+//     },
+//   },
+//   rules: {
+//     "interface-method-style/interface-method-style": "error",
+//   },
+//   languageOptions: {
+//     parser: typescriptParser,
+//     parserOptions: {
+//       projectService: true,
+//       tsconfigRootDir: process.cwd(),
+//     },
+//   },
+// } as const;
+
+const plugin = {
+  rules,
+  name: namePlugin,
+} as unknown as ESLint.Plugin;
+
+const config: Linter.Config = {
   plugins: {
-    "interface-method-style": {
-      meta: { name, version },
-      rules,
-    },
+    [namePlugin]: plugin,
   },
   rules: {
     "interface-method-style/interface-method-style": "error",
   },
-  languageOptions: {
-    parser: typescriptParser,
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: process.cwd(),
-    },
-  },
-} as const;
-
-const plugin = {
-  configs: {
-    flat: flatConfig,
-  },
-  rules,
 };
 
-export = plugin;
+// const legacyConfig = {};
+
+module.exports = {
+  ...plugin,
+  configs: {
+    recommended: config,
+  },
+};
