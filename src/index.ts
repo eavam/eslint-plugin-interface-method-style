83ea6beb2afb65545145e7627e62e4378aefec52
Egor Avakumov <16797465+eavam@users.noreply.github.com>
2024-11-30 16:13:07 +0300

    feat: restructure plugin export to include configs and enforce interface-method-style rule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import interfaceMethodStyle from "./interface-method-style.js";
 import { name, version } from "../package.json";
 
-export = {
+const basePlugin = {
   meta: { name, version },
   rules: {
     "interface-method-style": interfaceMethodStyle,
   },
+};
+
+export = {
+  configs: {
+    plugins: {
+      "interface-method-style": basePlugin,
+    },
+    rules: {
+      "@typescript-eslint/interface-method-style": "error",
+    },
+  },
+  ...basePlugin,
 };
