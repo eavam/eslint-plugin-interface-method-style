33ea9eeefd1e069b51d36e59e3998f0e95778b00
Egor Avakumov <16797465+eavam@users.noreply.github.com>
2025-07-14 16:51:46 +0300

    fix: exports for flat config

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,53 @@
 import interfaceMethodStyle from "./interface-method-style.js";
 import { type ESLint, type Linter } from "eslint";
+import { readFileSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const namePlugin = "interface-method-style";
 
-const rules = {
-  "interface-method-style": interfaceMethodStyle,
-};
+const pkg = JSON.parse(
+  readFileSync(
+    resolve(dirname(fileURLToPath(import.meta.url)), "../package.json"),
+    "utf8",
+  ),
+);
 
-const plugin = {
-  rules,
-  name: namePlugin,
-} as unknown as ESLint.Plugin;
+type PluginWithConfigs = ESLint.Plugin & { configs: Record<string, unknown> };
 
-const config: Linter.Config = {
-  plugins: {
-    [namePlugin]: plugin,
+const plugin: PluginWithConfigs = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+    namespace: namePlugin,
   },
+  rules: {
+    "interface-method-style": interfaceMethodStyle,
+  },
+  configs: {},
+} as unknown as PluginWithConfigs;
+
+const flatRecommended = [
+  {
+    plugins: {
+      [namePlugin]: plugin as unknown as ESLint.Plugin,
+    },
+    rules: {
+      "interface-method-style/interface-method-style": "error",
+    },
+  },
+] as unknown as Linter.Config[];
+
+const legacyRecommended = {
+  plugins: [namePlugin],
   rules: {
     "interface-method-style/interface-method-style": "error",
   },
-};
+} as unknown as Linter.Config;
 
-const pluginExport = {
-  ...plugin,
-  configs: {
-    recommended: config,
-  },
-};
+Object.assign(plugin.configs, {
+  "flat/recommended": flatRecommended,
+  recommended: legacyRecommended,
+});
 
-export default pluginExport;
+export default plugin;
